4b1a51003d0a63a68ae7d263b074c8bf7011b404
Matteo <ezmatteo@aol.com>
2024-01-17 09:13:12 +0100

    modifica index.js delle actions

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,31 +1,36 @@
-export const ADD_TO_FAVOURITE = 'ADD_TO_FAVOURITE'
-export const REMOVE_FROM_FAVOURITE = 'REMOVE_FROM_FAVOURITE'
-export const GET_JOBS = 'GET_JOBS'
+export const ADD_TO_PREF = "ADD_TO_PREF"
+export const REMOVE_FROM_PREF = "REMOVE_FROM_PREF"
+export const GET_WORKS = "GET_WORKS"
 
-const baseEndpoint = 'https://strive-benchmark.herokuapp.com/api/jobs?search='
+export const addToPrefAction = (workPref) => {
+  return {
+    type: ADD_TO_PREF, // è importante dare al tipo, e in generale è una buona pratica della naming convention, un nome parlante, che spieghi cosa fa/a cosa serve
+    payload: workPref,
+  }
+}
 
-export const addToFavouriteAction = (company) => ({
-  type: ADD_TO_FAVOURITE,
-  payload: company,
-})
+export const removeFromPrefAction = (i) => {
+  return {
+    type: REMOVE_FROM_PREF,
+    payload: i,
+  }
+}
 
-export const removeFromFavouriteAction = (company) => ({
-  type: REMOVE_FROM_FAVOURITE,
-  payload: company,
-})
-
-export const getJobsAction = (query) => {
+export const getWorksAction = (searchQuery) => {
   return async (dispatch) => {
+    //qui verrà effettuata la fetch (o qualsiasi operazione asincrona) che, una volta ottenuto il risultato, sarà spedita (dispatch) verso il reducer che farà il dispatch verso lo store e quindi verso i components
     try {
-      const response = await fetch(baseEndpoint + query + '&limit=20')
-      if (response.ok) {
-        const { data } = await response.json()
+      let resp = await fetch(
+        "https://strive-benchmark.herokuapp.com/api/jobs?company=" + searchQuery
+      )
+      if (resp.ok) {
+        let works = await resp.json()
         dispatch({
-          type: GET_JOBS,
-          payload: data,
+          type: GET_WORKS,
+          payload: works,
         })
       } else {
-        alert('Error fetching results')
+        throw new Error("Errore nel recupero dati")
       }
     } catch (error) {
       console.log(error)
